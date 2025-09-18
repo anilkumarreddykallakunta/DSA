7976971c12681f2ad464511c389ac49e79262ead
anilkumarreddykallakunta <160493793+anilkumarreddykallakunta@users.noreply.github.com>
2025-09-18 14:32:41 +0530

    Time: 1 ms (96.70%) | Memory: 66.3 MB (9.52%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let mini = prices[0];
-    let maxProfit =0;
-    for(let i =1;i<prices.length;i++){
-        let cost = prices[i] - mini;
-        maxProfit = Math.max(cost,maxProfit);
-        mini = Math.min(mini,prices[i]);
-
-    }
-    return maxProfit;
+  let  n = prices.length;
+  let maxProfit =0;
+  let mini = prices[0];
+  for(let i =1;i<n;i++){
+    let cost = prices[i] - mini;
+    maxProfit = Math.max(maxProfit,cost);
+    mini = Math.min(mini,prices[i]);
+  }  
+  return maxProfit;
 };
