e97f880d8be2e2ec3da3107b79e744035c82a95c
anilkumarreddykallakunta <160493793+anilkumarreddykallakunta@users.noreply.github.com>
2025-09-19 14:31:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (39.11%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int p1=0;
-        int p2=0;
-        vector<int> nums1Copy(nums1.begin(),nums1.begin() +m);
-        for(int i=0;i<m+n;i++){
-            if( p2>=n || (p1<m && nums1Copy[p1]<nums2[p2] )){
-                nums1[i] = nums1Copy[p1];
-                p1++;
+        int p1 = m-1;
+        int p2 = n-1;
+        for(int i=m+n-1;i>=0;i--){
+            if(p2<0){
+                break;
+            }
+            if(p1>=0 && nums1[p1]>nums2[p2]){
+                nums1[i] = nums1[p1];
+                p1--;
             }
             else{
                 nums1[i] = nums2[p2];
-                p2++;
+                p2--;
             }
         }
     }
