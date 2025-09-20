c9985d61d9d3fdd648262d9f42784c7a163252a8
anilkumarreddykallakunta <160493793+anilkumarreddykallakunta@users.noreply.github.com>
2025-09-20 16:10:54 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (55.39%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,12 +4,9 @@ public:
         int x =0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                nums[x] = nums[i];
+                swap(nums[x],nums[i]);
                 x++;
             }
-        }
-        for(int i =x;i<nums.size();i++){
-            nums[i] = 0;
         }
     }
 };
