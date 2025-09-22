7a2decf3ede55063a21612d813968fe72821829b
anilkumarreddykallakunta <160493793+anilkumarreddykallakunta@users.noreply.github.com>
2025-09-22 17:08:08 +0530

    Time: 0 ms (100.00%) | Memory: 57.2 MB (30.99%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let n = nums.length;
-    let sum = (n *(n+1))/2;
-    let total=0;
+    let  xor1=0;
+    let xor2=0;
     for(let i=0;i<nums.length;i++){
-        total = total + nums[i];
+        xor1 = xor1^nums[i];
+        xor2 = xor2^(i+1);
     }
-    return (sum - total);
+return xor1^xor2;
 };
