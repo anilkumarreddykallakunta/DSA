363f32a4e34a638241e4e0b37409daf448e53ce0
anilkumarreddykallakunta <160493793+anilkumarreddykallakunta@users.noreply.github.com>
2025-09-22 16:58:39 +0530

    Time: 10 ms (35.26%) | Memory: 58.9 MB (20.41%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,18 +3,19 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-  let hash = {};
-  for(let i=0;i<nums.length;i++){
-    if( !hash[nums[i]]){
-         hash[nums[i]]=1;
+    let hash = {};
+    let n = nums.length;
+    for(let i=0;i<n;i++){
+        if(!hash[nums[i]]){
+            hash[nums[i]] =1;
+        }
+        else{
+            hash[nums[i]]++;
+        }
     }
-    else{
-         hash[nums[i]]++;
+    for(let i=0;i<n;i++){
+        if(hash[nums[i]]==1){
+            return nums[i];
+        }
     }
-  }  
-  for(let i=0;i<nums.length;i++){
-    if(hash[nums[i]]==1){
-        return nums[i];
-    }
-  }
 };
