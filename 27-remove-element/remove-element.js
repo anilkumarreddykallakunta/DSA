0c1b8d1751a77041a71858080d07be9dbee75528
anilkumarreddykallakunta <160493793+anilkumarreddykallakunta@users.noreply.github.com>
2025-10-14 13:43:46 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (49.23%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,9 +4,8 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let  x =0;
-    let n = nums.length;
-    for(let i=0;i<n;i++){
+    let x=0;
+    for(let i =0;i<nums.length;i++){
         if(nums[i]!=val){
             nums[x] = nums[i];
             x++;
