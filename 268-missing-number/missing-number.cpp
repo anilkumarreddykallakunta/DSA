444791917a5539ce06d892e2eb7b1fdd1bd62681
anilkumarreddykallakunta <160493793+anilkumarreddykallakunta@users.noreply.github.com>
2025-11-06 19:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.69%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -5,9 +5,8 @@ public:
         int xor2 =0;
         for(int i=0;i<nums.size();i++){
             xor1 = xor1 ^ nums[i];
-            xor2  = xor2^ (i+1);
+            xor2 = xor2 ^ (i+1);
         }
-     
-        return xor1^xor2;
+        return xor1 ^ xor2;
     }
 };
